7038862fd43ad4620feb4f670df30c420b402760
Pachara6411361 <u6411361@au.edu>
2024-08-24 10:25:00 +0700

    increase SizeWarningLimit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase the limit to 1000 kB
+  },
 })

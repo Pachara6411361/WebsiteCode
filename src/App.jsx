365898a3a5c9eb91aa7105c02db3ea845f77a8b9
Phutthiphat <90144841+phutthiphathut@users.noreply.github.com>
2024-08-25 16:03:15 +0700

    update build files

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <HashRouter basename="/WebsiteProject/dist">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<DashboardPage />} />
         <Route path="/highlighted-cars" element={<HighlightedCarPage />} />
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/WebsiteProject/dist',
+  base: '/WebsiteProject',
   build: {
     chunkSizeWarningLimit: 1000, // Increase the limit to 1000 kB
   },

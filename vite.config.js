4039674f4d977215bcbc4edfb2c8121cf729463b
Phutthiphat <90144841+phutthiphathut@users.noreply.github.com>
2024-08-25 15:07:52 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Websitecode',
+  base: '/dist',
   build: {
     chunkSizeWarningLimit: 1000, // Increase the limit to 1000 kB
   },
